fbeea41b43d0a30f2359684f25084bf2a0e6d0df
HamdyD <hamdy.dakkak@gmail.com>
2019-01-22 16:22:26 +0100

    rendering reminders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { addReminder } from "../actions";
+import { addReminder, deleteReminder } from "../actions";
 
 class App extends Component {
   constructor(props) {
@@ -15,11 +15,38 @@ class App extends Component {
     this.props.addReminder(this.state.text);
   }
 
+  deleteReminder(id) {
+    console.log("deleting in application", id);
+    console.log("this.props", this.props);
+    this.props.deleteReminder(id);
+  }
+
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group col-sm-4">
+        {reminders.map(reminder => {
+          return (
+            <li key={reminder.id} className="list-group-item">
+              <div className="list-item">{reminder.text}</div>
+              <div
+                className="list-item delete-button"
+                onClick={() => this.deleteReminder(reminder.id)}
+              >
+                &#x2715;
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <div className="title">Reminder Pro</div>
-        <div className="form-inline">
+        <div className="form-inline reminder-form">
           <div className="form-group">
             <input
               className="form-control"
@@ -35,16 +62,23 @@ class App extends Component {
             Add Reminder
           </button>
         </div>
+        {this.renderReminders()}
       </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  };
+}
+
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ addReminder }, dispatch);
+  return bindActionCreators({ addReminder, deleteReminder }, dispatch);
 }
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
